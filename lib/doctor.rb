d3ba1bb863578462e894db8160f6ed94fb2629fb
Sav Younts <savyounts@gmail.com>
2018-08-10 20:52:49 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,4 +2,13 @@ class Doctor
   attr_accessor :name 
   @@all = []
   
-  def in
+  def initialize(name) 
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def new_appointment()
