6b3c4f50508323ae63b60cb4d2c0773d25b7ee12
Sav Younts <savyounts@gmail.com>
2018-08-10 21:13:15 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -20,6 +20,6 @@ class Doctor
   end 
   
   def patients 
-    self.appointments.collect {|appointment| appointment.patient}
+    self.appointments.collect {|appointment| appointment.date}
   end
 end
