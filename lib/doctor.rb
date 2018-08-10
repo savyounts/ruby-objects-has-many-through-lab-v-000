62998470aab959dfd38930c37d58de5075dbccfc
Sav Younts <savyounts@gmail.com>
2018-08-10 21:00:18 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
   end 
   
   def appointments 
-    Appointment.all.select {|appointment| appointment.doctor == self 
+    Appointment.all.select {|appointment| appointment.doctor == self }
   end 
   
   def patients 
