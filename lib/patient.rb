bb5f0f10fc87b07763265d1cc8b3816c2ba69ec6
Sav Younts <savyounts@gmail.com>
2018-08-10 21:06:21 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,7 +12,7 @@ class Patient
   end 
   
   def new_appointment(date, doctor)
-    date = Appointment.new(date, doctor, self)
+    Appointment.new(date, doctor, self)
   end 
   
   def appointments
