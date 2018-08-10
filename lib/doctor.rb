dc91d2140e30aa4428da467ebd0bcb15cae84fbb
Sav Younts <savyounts@gmail.com>
2018-08-10 20:54:21 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,4 +11,9 @@ class Doctor
     @@all 
   end
   
-  def new_appointment()
+  def new_appointment(date, patient)
+    date = Appointment.new(date, self, patient)
+  end 
+  
+  def appointments 
+    Appointment.all.select {|appointment| appointment.doctor ==
